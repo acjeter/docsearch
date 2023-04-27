2e51be51fa6b7dd90e74f7b7496728b8b5788cfd
Andrew Jeter <119645659+acjeter@users.noreply.github.com>
2023-04-26 19:04:42 -0700

    Lab 4 Work

--- TestDocSearch.java
+++ TestDocSearch.java
@@ -8,15 +8,15 @@ public class TestDocSearch {
 	@Test 
 	public void testIndex() throws URISyntaxException, IOException {
     Handler h = new Handler("./technical/");
-    URI rootPath = new URI("http://localhost/");
-    assertEquals("There are 224 total files to search.", h.handleRequest(rootPath));
+    URI rootPath = new URI("https://localhost/");
+    assertEquals("There are 1 total files to search.", h.handleRequest(rootPath));
 	}
-	@Test 
-	public void testSearch() throws URISyntaxException, IOException {
-    Handler h = new Handler("./technical/");
-    URI rootPath = new URI("http://localhost/search?q=sushi");
-    String expect = "Found 3 paths:\n./written_2/travel_guides/berlitz1/WhatToJapan.txt\n./written_2/travel_guides/berlitz1/WhereToJapan.txt\n./written_2/travel_guides/berlitz2/California-WhereToGo.txt";
-    assertEquals(expect, h.handleRequest(rootPath));
-	}
+	// @Test 
+	// public void testSearch() throws URISyntaxException, IOException {
+    // Handler h = new Handler("./technical");
+    // URI rootPath = new URI("http://localhost/search?q=base%20pair");
+    // String expect = "Found 3 paths:\n./technical/plos/journal.pbio.0020190.txt\n./technical/plos/journal.pbio.0020190.txt\n./technical/plos/journal.pbio.0020223.txt";
+    // assertEquals(expect, h.handleRequest(rootPath));
+	//}
 }
 
